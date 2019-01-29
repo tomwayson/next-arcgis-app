9294718e5d46986aed64a41823abd864a9bfcf38
Tom Wayson <tom@tomwayson.com>
2019-01-29 13:04:21 -0800

    only issue a warning for missing prop-types linting rule

--- components/AppNav.js
+++ components/AppNav.js
@@ -22,7 +22,7 @@ class AppNav extends React.Component {
     });
   };
   render() {
-    // const { userMenu } = this.props;
+    const { userMenu } = this.props;
     // TODO: show active link based on the current route
     // see: https://github.com/zeit/next.js/#using-a-higher-order-component
     return (
