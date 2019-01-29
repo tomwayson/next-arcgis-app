a789487e9a0905f3408f64fb815935928639ef84
Tom Wayson <tom@tomwayson.com>
2019-01-29 13:11:21 -0800

    added home route

--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,23 @@
-// eslint-disable-next-line
-export default () => <div>TODO: home page goes here.</div>
+import React from 'react';
+import Router from 'next/router';
+import AgoSearch from '../components/AgoSearch';
+
+function index() {
+  function onSearch(q) {
+    // pass the search term to the items page
+    Router.push({
+      pathname: '/items',
+      query: { q }
+    });
+  }
+  return (
+    <div className="jumbotron">
+      <h1 className="display-3 text-light text-center mb-5">
+        Ambitious ArcGIS App
+      </h1>
+      <AgoSearch q="" onSearch={onSearch} size="lg" />
+    </div>
+  );
+}
+
+export default index;
