a6d71cc697af31e7b4ba6fad15c75c88051688e5
Tom Wayson <tom@tomwayson.com>
2019-01-29 16:24:49 -0800

    fetch items using arcgis-rest-js, and render results as a table

---
+++ components/ItemsLayout.js
@@ -0,0 +1,56 @@
+import React from 'react';
+// import { ItemPager } from 'react-arcgis-hub';
+import AgoSearch from './AgoSearch';
+// import ExtentsMap from './ExtentsMap';
+import ItemsTable from './ItemsTable';
+
+function ItemsLayout({ results, total, num, q, start, onParamsChange }) {
+  function onSearch(q) {
+    if (onParamsChange) {
+      onParamsChange(q);
+    }
+  }
+  // function changePage(page) {
+  //   // calculate next start record based on the number of records per page
+  //   const start = (page - 1) * num + 1;
+  //   if (onParamsChange) {
+  //     onParamsChange(q, start);
+  //   }
+  // }
+  // compute current page number based on start record
+  // and the number of records per page
+  const pageNumber = (start - 1) / num + 1;
+  return (
+    <>
+      <div className="row mb-2">
+        <div className="col-9">
+          <h2>
+            Your search for "{q}" yielded {total} items
+          </h2>
+        </div>
+        <div className="col-3">
+          <AgoSearch
+            q={q}
+            onSearch={onSearch}
+            className="search-form-inline"
+            size="sm"
+          />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12">
+          {/* <ExtentsMap items={results} /> */}
+          <ItemsTable items={results} />
+          {/* <ItemPager
+            pageSize={num}
+            totalCount={total}
+            pageNumber={pageNumber}
+            changePage={changePage}
+          /> */}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ItemsLayout;
--- pages/items.js
+++ pages/items.js
@@ -1,12 +1,69 @@
 import React from 'react';
+import Router from 'next/router';
+import { searchItems } from '@esri/arcgis-rest-items';
+import ItemsLayout from '../components/ItemsLayout';
 
 class Items extends React.Component {
-  static async getInitialProps({ query }) {
-    const q = query && query.q;
-    return { q };
+  // this is called on the server for initial render
+  // or on the client when the route params change
+  // see: https://nextjs.org/docs#fetching-data-and-component-lifecycle
+  static async getInitialProps({ pathname, query }) {
+    const defaults = {
+      num: 10,
+      start: 1
+    };
+    const searchForm = { ...defaults, ...query };
+    // execute search and update state
+    return searchItems({
+      searchForm //,
+      // TODO: pass session too
+      // authentication
+    })
+      .then(({ results, total }) => {
+        return {
+          pathname,
+          error: null,
+          results,
+          total,
+          ...searchForm
+        };
+      })
+      .catch(e => {
+        this.setState({
+          pathname,
+          error: e.message || e,
+          results: null,
+          total: 0,
+          ...searchForm
+        });
+      });
   }
+  onParamsChange = (q, start) => {
+    // update the route query params after the user either
+    // submits the inline search form or links to a new page
+    const { pathname } = this.props;
+    const query = { q };
+    if (start) {
+      query.start = start;
+    }
+    Router.push({
+      pathname,
+      query
+    });
+  };
   render() {
-    return <div>TODO: search for: {this.props.q}</div>;
+    // these props are returned from getInitialProps
+    const { results, total, num, q, start } = this.props;
+    return (
+      <ItemsLayout
+        results={results}
+        total={total}
+        num={num}
+        q={q}
+        start={start}
+        onParamsChange={this.onParamsChange}
+      />
+    );
   }
 }
 
