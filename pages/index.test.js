0bfc9eff4b1f57ff973db06712a0aa91f126a2fa
Tom Wayson <tom@tomwayson.com>
2019-02-01 08:06:18 -0800

    add snapshot test

--- pages/index.test.js
+++ pages/index.test.js
@@ -4,9 +4,13 @@ import Index from './index';
 
 describe('smoke tests', function() {
   describe('w/o a previous session', function() {
-    it('renders app title and sign in button', () => {
+    it('renders app title', () => {
       const { getByText } = render(<Index />);
       expect(getByText('Ambitious ArcGIS App')).toBeInTheDocument();
+    });
+    it('matches snapshot', () => {
+      const { asFragment } = render(<Index />);
+      expect(asFragment()).toMatchSnapshot();
     });
   });
 });
